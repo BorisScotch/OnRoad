c15ff220c7e909ee31ff87199e13746377ff8d5a
Boris <boris.coalova@gmail.com>
2017-05-22 18:30:25 -0300

    Update svg.js

--- svg.js
+++ svg.js
@@ -3,42 +3,114 @@
 //------------------------------------------ MANGA IZQUIERDA ------------------------------------------
 function redmangaizquierda() {
  document.getElementById("mangaizquierda").setAttribute("fill", "DarkRed ");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+
 }
 function bluemangaizquierda() {
  document.getElementById("mangaizquierda").setAttribute("fill", "DarkBlue ");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+
 }
 function greenmangaizquierda() {
  document.getElementById("mangaizquierda").setAttribute("fill", "DarkGreen ");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+
 }
 function yellowmangaizquierda() {
  document.getElementById("mangaizquierda").setAttribute("fill", "GoldenRod ");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+ 
 }
 function DarkVioletmangaizquierda() {
  document.getElementById("mangaizquierda").setAttribute("fill", "DarkViolet ");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+
 }
 
 function Darkorangemangaizquierda() {
  document.getElementById("mangaizquierda").setAttribute("fill", "DarkOrange ");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+
+}
+function aquamarinemangaizquierda() {
+ document.getElementById("mangaizquierda").setAttribute("fill", "Aquamarine");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+
+}
+function blackmangaizquierda() {
+ document.getElementById("mangaizquierda").setAttribute("fill", "Black");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "white");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "white");
+}
+function DarkCyanmangaizquierda() {
+ document.getElementById("mangaizquierda").setAttribute("fill", "DarkCyan");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+
+}
+function Silvermangaizquierda() {
+ document.getElementById("mangaizquierda").setAttribute("fill", "Silver");
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
 }
 
-//------------------------------------------ MANGA DERECHA ------------------------------------------
+//------------------------------------------ MANGA DERECHA ------------------------------------------ 
 function redmangaderecha() {
  document.getElementById("mangaderecha").setAttribute("fill", "DarkRed ");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
 }
 function bluemangaderecha() {
  document.getElementById("mangaderecha").setAttribute("fill", "DarkBlue ");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
 }
 function greenmangaderecha() {
  document.getElementById("mangaderecha").setAttribute("fill", "DarkGreen ");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
 }
 function yellowmangaderecha() {
  document.getElementById("mangaderecha").setAttribute("fill", "GoldenRod ");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
 }
 function DarkVioletmangaderecha() {
  document.getElementById("mangaderecha").setAttribute("fill", "DarkViolet ");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
 }
 function darkorangemangaderecha() {
  document.getElementById("mangaderecha").setAttribute("fill", "DarkOrange ");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
+}
+function aquamarinemangaderecha() {
+ document.getElementById("mangaderecha").setAttribute("fill", "Aquamarine");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
+}
+function blackmangaderecha() {
+ document.getElementById("mangaderecha").setAttribute("fill", "Black");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "white");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "white");
+}
+function DarkCyanmangaderecha() {
+ document.getElementById("mangaderecha").setAttribute("fill", "DarkCyan");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
+}
+function Silvermangaderecha() {
+ document.getElementById("mangaderecha").setAttribute("fill", "Silver");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black");
 }
 
 //------------------------------------------ INTERIOR IZQUIERDO ------------------------------------------
@@ -60,6 +132,18 @@ function darkvioletinteriorizquierdo() {
 function darkorangeinteriorizquierdo() {
  document.getElementById("interiorizquierdo").setAttribute("fill", "DarkOrange ");
 }
+function aquamarineinteriorizquierdo() {
+ document.getElementById("interiorizquierdo").setAttribute("fill", "Aquamarine");
+}
+function blackinteriorizquierdo() {
+ document.getElementById("interiorizquierdo").setAttribute("fill", "Black");
+}
+function darkcyaninteriorizquierdo() {
+ document.getElementById("interiorizquierdo").setAttribute("fill", "DarkCyan");
+}
+function silverinteriorizquierdo() {
+ document.getElementById("interiorizquierdo").setAttribute("fill", "Silver");
+}
 
 //------------------------------------------ INTERIOR DERECHO ------------------------------------------
 function redinteriorderecho() {
@@ -80,7 +164,107 @@ function darkvioletinteriorderecho() {
 function darkorangeinteriorderecho() {
  document.getElementById("interiorderecho").setAttribute("fill", "DarkOrange ");
 }
+function aquamarineinteriorderecho() {
+ document.getElementById("interiorderecho").setAttribute("fill", "Aquamarine");
+}
+function blackinteriorderecho() {
+ document.getElementById("interiorderecho").setAttribute("fill", "Black");
+}
+function darkcyaninteriorderecho() {
+ document.getElementById("interiorderecho").setAttribute("fill", "DarkCyan");
+}
+function silverinteriorderecho() {
+ document.getElementById("interiorderecho").setAttribute("fill", "Silver");
+}
 
+//------------------------------------------ BOLSILLO DERECHO ------------------------------------------
+ 
+function redbolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "DarkRed ");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function bluebolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "DarkBlue ");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function greenbolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "DarkGreen ");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function yellowbolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "GoldenRod ");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function darkvioletbolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "DarkViolet ");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function darkorangebolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "DarkOrange ");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function aquamarinebolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "Aquamarine");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function blackinterbolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "Black");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "white ");
+}
+function darkcyanbolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "DarkCyan");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+function silverbolsilloderecho() {
+ document.getElementById("bolsilloderecho").setAttribute("fill", "Silver");
+ document.getElementById("bolsilloderechoagujero").setAttribute("fill", "Black ");
+}
+
+
+//------------------------------------------ BOLSILLO IZQUIERDO ------------------------------------------
+
+ function redbolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "DarkRed ");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function bluebolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "DarkBlue ");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function greenbolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "DarkGreen ");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function yellowbolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "GoldenRod ");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function darkvioletbolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "DarkViolet ");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function darkorangebolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "DarkOrange ");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function aquamarinebolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "Aquamarine");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function blackinterbolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "Black");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "white ");
+}
+function darkcyanbolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "DarkCyan");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+function silverbolsilloizquierdo() {
+ document.getElementById("bolsilloizquierdo").setAttribute("fill", "Silver");
+ document.getElementById("bolsilloizquierdoagujero").setAttribute("fill", "Black ");
+}
+
+ 
 //------------------------------------------ CAPUCHA ------------------------------------------
 function redcapucha() {
  document.getElementById("capucha").setAttribute("fill", "DarkRed ");
@@ -118,7 +302,30 @@ function darkorangecapucha() {
  document.getElementById("capucha3").setAttribute("fill", "DarkOrange ");
  document.getElementById("capucha4").setAttribute("fill", "DarkOrange ");
 }
-
+function aquamarinecapucha() {
+ document.getElementById("capucha").setAttribute("fill", "Aquamarine ");
+ document.getElementById("capucha2").setAttribute("fill", "Aquamarine ");
+ document.getElementById("capucha3").setAttribute("fill", "Aquamarine ");
+ document.getElementById("capucha4").setAttribute("fill", "Aquamarine ");
+}
+function blackcapucha() {
+ document.getElementById("capucha").setAttribute("fill", "Black ");
+ document.getElementById("capucha2").setAttribute("fill", "Black ");
+ document.getElementById("capucha3").setAttribute("fill", "Black ");
+ document.getElementById("capucha4").setAttribute("fill", "Black ");
+}
+function darkcyancapucha() {
+ document.getElementById("capucha").setAttribute("fill", "DarkCyan ");
+ document.getElementById("capucha2").setAttribute("fill", "DarkCyan ");
+ document.getElementById("capucha3").setAttribute("fill", "DarkCyan ");
+ document.getElementById("capucha4").setAttribute("fill", "DarkCyan ");
+}
+function silvercapucha() {
+ document.getElementById("capucha").setAttribute("fill", "Silver ");
+ document.getElementById("capucha2").setAttribute("fill", "Silver ");
+ document.getElementById("capucha3").setAttribute("fill", "Silver ");
+ document.getElementById("capucha4").setAttribute("fill", "Silver ");
+}
 
 //------------------------------------------ CAPUCHA INTERIOR ------------------------------------------
 function redcapuchainterior() {
@@ -145,7 +352,22 @@ function darkorangecapuchainterior() {
  document.getElementById("capuchainterior").setAttribute("fill", "DarkOrange ");
  document.getElementById("capuchainterior2").setAttribute("fill", "DarkOrange ");
 }
-
+function aquamarinecapuchainterior() {
+ document.getElementById("capuchainterior").setAttribute("fill", "Aquamarine ");
+ document.getElementById("capuchainterior2").setAttribute("fill", "Aquamarine ");
+}
+function blackcapuchainterior() {
+ document.getElementById("capuchainterior").setAttribute("fill", "Black ");
+ document.getElementById("capuchainterior2").setAttribute("fill", "Black ");
+}
+function darkcyancapuchainterior() {
+ document.getElementById("capuchainterior").setAttribute("fill", "DarkCyan ");
+ document.getElementById("capuchainterior2").setAttribute("fill", "DarkCyan ");
+}
+function silvercapuchainterior() {
+ document.getElementById("capuchainterior").setAttribute("fill", "Silver ");
+ document.getElementById("capuchainterior2").setAttribute("fill", "Silver ");
+}
 
 //------------------------------------------ RESET ------------------------------------------
 function resetsall() {
@@ -159,7 +381,62 @@ function resetsall() {
  document.getElementById("capucha4").setAttribute("fill", "white ");
  document.getElementById("capuchainterior").setAttribute("fill", "white ");
  document.getElementById("capuchainterior2").setAttribute("fill", "white ");
-
-
+ document.getElementById("arrugasmangaizquierda").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaizquierda2").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha").setAttribute("fill", "black");
+ document.getElementById("arrugasmangaderecha2").setAttribute("fill", "black"); 
 }
 
+//------------------------------------------ MENU COLORES ------------------------------------------
+var MainMenu = (function() {
+    var MainMenu = function(config) {
+        config = config || {};
+        this.toggleBtn = $(config.toggleBtn);
+        this.menu = $(config.menu);
+        this.close = $(config.close);
+
+        this.init();
+        config = null;
+    };
+    // public interface
+    MainMenu.prototype = {
+        constructor: MainMenu,
+        init: function() {
+            this.eventManager();
+        },
+        eventManager: function() {
+            this.toggleBtn.on('click.openMenu', onButtonClickHandler.bind(this));
+            this.close.on('click.closeMenu', onCloseClickHandler.bind(this));
+        }
+    };
+    // private interface
+    function onButtonClickHandler(menu, evt) {
+        if (!this.menu.hasClass('open')) {
+            this.menu.addClass('open');
+        };
+
+    }
+
+    function onCloseClickHandler(evt) {
+        this.menu.removeClass('open')
+    }
+
+    function onDocumentClickHandler(evt) {
+        var $target = $(evt.target);
+
+        if (!$target.closest(this.menuForm).length && !$target.closest(this.menuContent).length && this.menu.hasClass('open')) {
+            this.menu.removeClass('open')
+        }
+    }
+
+    return MainMenu;
+})();
+
+
+$(document).ready(function() {
+    var mainMenu = new MainMenu({
+        menu: '.main-menu',
+        toggleBtn: '.main-menu-btn',
+        close: '.main-menu-close'
+    });
+});
